bcd2a1c9bc6b79823385acedac416ea808910425
VTshk <46195996+VTshk@users.noreply.github.com>
2020-04-09 07:02:06 +0300

    create app

---
+++ src/views/Messaging.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="Messaging">
+    <DialogList/>
+    <MessageList/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import DialogList from '@/components/Messaging/DialogList/DialogList.vue';
+import MessageList from '@/components/Messaging/MessageList/MessageList.vue';
+
+export default {
+  name: 'Messaging',
+  components: {
+    DialogList,
+    MessageList,
+  },
+};
+</script>
